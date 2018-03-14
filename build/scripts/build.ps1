7d74bc28f81af55783dccb9905ce582c90663165
Sietse <sietsevdschoot@gmail.com>
2018-03-14 22:47:48 +0100

    Added build script to generate and push a new version if the package has changed..

--- build/scripts/build.ps1
+++ build/scripts/build.ps1
@@ -1,30 +1,5 @@
-$token = "oy2kgezuhcxlfy6eouoqgrmfm52dog3e7dh4ebf4wh7444"
-
-$url = "https://api.nuget.org/v3/registration3/automapper/index.json"
-
-
-
-
-
-function Get-PackageInfoFromFeed {
-    param()
-
-    $packageFeed = "https://api-v2v3search-0.nuget.org/query?q=packageid:newtonsoft.Json"
-
-    $response = Invoke-RestMethod -Uri $packageFeed -Verbose:$false
-
-    if ($response.data -eq $null) {
-    
-        Throw "No packages found.."
-    }
-
-    [PsCustomObject]@{ `
-        PackageId = $response.data.id; `
-        Version = $response.data.version; `
-        Description = ($response.data.description.Split('|') | Select -last 1).Trim(); `
-    }
-}
-
+$solutionRoot = (gi $PSScriptRoot\..\..\).FullName
+$nugetApiKey = (cat "$solutionRoot\..\Credentials\ExceptionLayoutFormatterNuget.txt" -Raw -ErrorAction SilentlyContinue).Trim()
 
 Function Get-ProjectInfo {
     param(
@@ -48,12 +23,106 @@ Function Get-ProjectInfo {
     }    
 }
 
-$solutionRoot = "$PSScriptRoot\..\..\"  
+function Get-PackageInfoFromFeed {
+    param(
+        [string] $packageId
+    )
 
-$packageInfo = Get-PackageInfoFromFeed
-$projectInfo = Get-ProjectInfo "$solutionRoot\src\ExceptionLayoutFormatter\ExceptionLayoutFormatter.csproj"
+    $packageFeed = "https://api-v2v3search-0.nuget.org/query?q=packageid:$packageId"
 
-$packageInfo | ft
-$projectInfo | ft
+    $response = Invoke-RestMethod -Uri $packageFeed -Verbose:$false
 
+    if ($response.data -eq $null) {
+    
+        Throw "No packages found.."
+    }
 
+    [PsCustomObject]@{ `
+        PackageId = $response.data.id; `
+        Version = $response.data.version; `
+        Description = $response.data.description; `
+    }
+}
+
+Function SkipOrPushNewPackage {
+    param (
+        [IO.FileInfo] $projectFile
+    )
+
+    $projectInfo = Get-ProjectInfo $projectFile
+    $publishedPackage = Get-PackageInfoFromFeed -packageId $projectInfo.PackageId
+
+    $hasPackageChanged = $true
+
+    if ($publishedPackage) {
+
+        $publishedPackageHash = $null
+        if ($publishedPackage.Description.Contains("|")) 
+        {
+            $publishedPackageHash = ($publishedPackage.Description.Split("|") | Select -Last 1).Trim()
+        }
+        
+        #if the hash no longer matches, we should push a new package
+        $hasPackageChanged = ($projectInfo.Hash -ne $publishedPackageHash)
+    }
+
+    if ($hasPackageChanged) {
+
+        PublishNewPackage -projectInfo $projectInfo -publishedPackage $publishedPackage
+    }
+    else {
+    
+        Write-Host " [SKIP] $($projectInfo.PackageId) has not changed"
+    }
+} 
+
+Function PublishNewPackage {
+    param (
+        $projectInfo,
+        $publishedPackage
+    )
+
+    $newVersion = Get-NewPackageVersionNumber -currentVersion $($publishedPackage.Version)
+
+    $newDescription = ("{0} | {1}" -f $projectInfo.Description, $projectInfo.Hash) 
+
+    dotnet pack $($projectFile.FullName) `
+        --include-symbols --include-source --no-build `
+        /p:PackageVersion=$newVersion /p:Description=$newDescription --output $solutionRoot\build | Out-String | Write-Verbose
+        
+    $isSuccess = $LASTEXITCODE -eq 0
+
+    $generatedPackage = [IO.Path]::Combine($solutionRoot,"build","$($projectInfo.PackageId).$newVersion.symbols.nupkg")
+    
+    if ($nugetApiKey) {
+
+        NuGet.exe push $generatedPackage -ApiKey $nugetApiKey -source https://api.nuget.org/v3/index.json | Out-String | Write-Verbose
+        
+        $isSuccess = $isSuccess -and $LASTEXITCODE -eq 0
+        
+        if ($isSuccess) {
+        
+            Write-Host " [DONE] Pushed $($projectInfo.PackageId) $newVersion $(($newDescription.Split('|') | Select -Last 1).Trim()) to NuGet Feed"  
+        }
+        else {
+        
+            Write-Host " [FAIL] Creating / Pushing $($projectInfo.PackageId) $newVersion $(($newDescription.Split('|') | Select -Last 1).Trim()) "  
+        }
+    }
+    elseif ($isSuccess) {
+    
+        Write-Host " [DONE] Created $generatedPackage" 
+    }
+}
+
+Function Get-NewPackageVersionNumber {
+    param (
+        [string] $currentVersion
+    )
+
+    $version = [version]$currentVersion
+
+    ("{0}.{1}.{2}.{3}" -f $version.Major, $version.Minor, $version.Build, ([Math]::Max($version.Revision, 0) + 1))
+}
+
+SkipOrPushNewPackage "$solutionRoot\src\ExceptionLayoutFormatter\ExceptionLayoutFormatter.csproj"
