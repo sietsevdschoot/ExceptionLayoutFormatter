e3dd49925e29f21b40da3f376f37da251a3e13cd
Sietse <sietsevdschoot@gmail.com>
2022-10-09 22:13:46 +0400

    Updated framework- and nuget versions.

--- tests/unitTests/Extensions/FluentAssertionsExtensions.cs
+++ tests/unitTests/Extensions/FluentAssertionsExtensions.cs
@@ -24,24 +24,23 @@ namespace UnitTests.Extensions
             subject.Should().BeEquivalentTo(expectation, newConfig, because, becauseArgs);
         }
     }
-    
+
     public class CompareStringsUsingCaseInsensitiveWildcards : IEquivalencyStep
     {
-        public bool CanHandle(IEquivalencyValidationContext context, IEquivalencyAssertionOptions config)
+        public EquivalencyResult Handle(Comparands comparands, IEquivalencyValidationContext context,
+            IEquivalencyValidator nestedValidator)
         {
-            Type expectationType = config.GetExpectationType(context);
+            if (comparands.GetExpectedType(new EquivalencyAssertionOptions()) != typeof(string))
+            {
+                return EquivalencyResult.ContinueWithNext;
+            }
+            
+            var subject = (string)comparands.Subject;
+            var expectation = (string)comparands.Expectation;
 
-            return (expectationType != null) && (expectationType == typeof(string));
-        }
+            subject.Should().MatchEquivalentOf(expectation, context.Reason.FormattedMessage, context.Reason.Arguments);
 
-        public bool Handle(IEquivalencyValidationContext context, IEquivalencyValidator parent, IEquivalencyAssertionOptions config)
-        {
-            string subject = (string)context.Subject;
-            string expectation = (string)context.Expectation;
-
-            subject.Should().MatchEquivalentOf(expectation, context.Because, context.BecauseArgs);
-
-            return true;
+            return EquivalencyResult.AssertionCompleted;
         }
     }
 }
